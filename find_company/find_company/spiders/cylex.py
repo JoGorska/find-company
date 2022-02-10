57eee33ca24cb64e57ca340c395174330ee1ef98
Joanna Gorska <gorska919@gmail.com>
2022-02-10 23:43:38 +0000

    Another attempt to fix 403 error, failed

--- find_company/find_company/spiders/cylex.py
+++ find_company/find_company/spiders/cylex.py
@@ -4,6 +4,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from find_company.items import Company
 import requests
+from urllib.request import Request, urlopen
+
 
 class CylexSpider(scrapy.Spider):
     '''
@@ -12,13 +14,16 @@ class CylexSpider(scrapy.Spider):
     name = 'cylex'
     allowed_domains = ['cylex-uk.co.uk']
     start_urls = ['https://corby.cylex-uk.co.uk/company/alexs-15659626.html']
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36',
-        'cookie': 'your cookie',
-    }
+    # headers = {
+    #     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36',
+    #     'cookie': 'your cookie',
+    # }
 
-    response = requests.get('https://corby.cylex-uk.co.uk/company/alexs-15659626.html', headers=headers)
+    # response = requests.get('https://corby.cylex-uk.co.uk/company/alexs-15659626.html', headers=headers)
     
+    req = Request('https://corby.cylex-uk.co.uk/company/alexs-15659626.html', headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(req).read()
+
     def parse_info(self, response):
         '''
         gets company name from the meta tag with property og:title
