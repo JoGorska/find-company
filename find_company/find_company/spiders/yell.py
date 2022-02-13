5d9238ea5f4b078352921483f838ca62e12b6ffc
Joanna Gorska <gorska919@gmail.com>
2022-02-13 14:55:30 +0000

    Attempt to use sitemaps to crawl - failed due to 503 error

--- find_company/find_company/spiders/yell.py
+++ find_company/find_company/spiders/yell.py
@@ -1,18 +1,22 @@
 #pylint: disable=W0223
 #pylint: disable=W0221
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
 from scrapy.linkextractors import LinkExtractor
 
 
-class YellSpider(scrapy.Spider):
+class YellSpider(SitemapSpider):
     '''
     spider to obtain company names of yell
     '''
     name = 'yell'
     allowed_domains = ['yell.com']
-    start_urls = ['https://www.yell.com/biz/automotive-solutions-corby-ltd-corby-9911336/']
+    sitemap_urls = ['https://www.yell.com/sitemaps.xml']
+    # start_urls = ['https://www.yell.com/biz/automotive-solutions-corby-ltd-corby-9911336/']
 
+    # rules = [
+    #     Rule(LinkExtractor(allow=r'cylex-uk.co.uk/company\/[a-zA-Z\-]+\/[a-zA-Z\-]+[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]+.html'), callback='parse_item', follow=True)
+    # ]
     def parse(self, response):
 
         company_name = response.xpath('//title/text()').extract_first()
