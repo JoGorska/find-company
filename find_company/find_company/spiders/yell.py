839482e6461988128025596e166b07edfbbe8789
Joanna Gorska <gorska919@gmail.com>
2022-02-13 20:04:55 +0000

    Tidy up Yell spider

--- find_company/find_company/spiders/yell.py
+++ find_company/find_company/spiders/yell.py
@@ -29,33 +29,5 @@ class YellSpider(scrapy.Spider):
             item['company_id'] = company_id
             item['company_name'] = company_name
             item['company_website'] = company_website
-            # print(f'WHAT DO WE HAVE INSIDE ITEM {item}')
             yield item
-        # print(f'ITEM LIST HERE {items_list}')
-        # for item in items_list:
-        #     yield item
 
-
-    # def parse(self, response):
-    #     item = FindCompanyItem()
-    #     all_containers = response.css('article').xpath('@role="listitem"').extract()
-    #     items_list = []
-    #     i = 0
-    #     for i in range(0, 2):
-    #         company_name = response.css('h2.businessCapsule--name::text').extract()[i]
-    #         company_website = (
-    #             if (response.xpath('//div/a[text()[contains(., "Website")]]/@href').extract()[i]):
-    #                 return response.xpath('//div/a[text()[contains(., "Website")]]/@href').extract()[i]
-    #             else:
-    #                 return response.css('h2.businessCapsule--name::text').extract()[i]
-    #             )
-    #         item['company_name'] = company_name
-    #         item['company_website'] = company_website
-    #         items_list.append(item)
-    #     for item in items_list:
-    #         yield item
-
-            # company_name = container.css('h2.businessCapsule--name::text').extract()
-            # company_website = container.css('div').xpath('//div/a[text()[contains(., "Website")]]/@href').extract()
-            # item['company_name'] = company_name
-            # item['company_website'] = company_website
