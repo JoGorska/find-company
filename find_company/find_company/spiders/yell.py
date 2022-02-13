c2645eff6be80de78b2f00bab454b7f54c0d8b8f
Joanna Gorska <gorska919@gmail.com>
2022-02-13 14:02:23 +0000

    Tidy up the spider

--- find_company/find_company/spiders/yell.py
+++ find_company/find_company/spiders/yell.py
@@ -12,21 +12,10 @@ class YellSpider(scrapy.Spider):
     start_urls = ['https://www.yell.com/biz/automotive-solutions-corby-ltd-corby-9911336/']
 
     def parse(self, response):
-        # using css selector
-        # company_name = response.css('title::text').extract_first()
-        # company_website = response.css('a.businessCard--callToAction').extract()
 
-        # xpath selector
         company_name = response.xpath('//title/text()').extract_first()
-
-        # xpath, but I don't like result 
-        # https://www.automotivesolutionscorby.com/?utm_source=yell&utm_medium=referral&utm_campaign=yell'
         company_website = response.xpath('//a[@itemprop="url"]/@href').extract_first()
      
-        # mix
-        # company_website = response.css('nav a').xpath('@href').extract()
-        # .xpath('//a[@class="businessCard--callToAction"]').extract()
-      
         yield {
             'company_name': company_name,
             'company_website': company_website,
